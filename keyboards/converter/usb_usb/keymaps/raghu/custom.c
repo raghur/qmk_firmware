bf62aae86bc978b5839d7a5ebb94fc78063f8620
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2022-06-22 19:27:58 +0530

    remove layer, avr size golf; add reset key

--- keyboards/converter/usb_usb/keymaps/raghu/custom.c
+++ keyboards/converter/usb_usb/keymaps/raghu/custom.c
@@ -1,3 +1,4 @@
+#include QMK_KEYBOARD_H
 
 /* const key_override_t brightness_up_override = ko_make_with_layers_negmods_and_options(MOD_MASK_CA, KC_MPLY, */
 /*                                             KC_BRIU, ~0, MOD_MASK_SHIFT, ko_option_no_reregister_trigger); */
@@ -8,14 +9,49 @@
 /*     &brightness_down_override, */
 /*     NULL */
 /* }; */
+
+#ifdef TAP_DANCE_ENABLE
 // Tap Dance declarations
 enum {
-    TD_PS_3,
+    TD_PS_2,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap PS once for print screen, double tap to toggle mousekeys
-    [TD_PS_3] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_PSCR, 3),
+    [TD_PS_2] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_PSCR, 2),
 };
+#endif
 
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+}
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("LYR: "), false);
+    /* oled_write_P(get_u8_str(get_highest_layer(layer_state), ' '), false); */
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("DFLT\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("XTRA\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("MOUSE\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("NA"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    /* oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false); */
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    /* oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false); */
+    
+    return false;
+}
+#endif
