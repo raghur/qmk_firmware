ccd98baf36b87356f486e67a85d8979b6e361790
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2022-08-04 14:00:21 +0530

    organize rules.mk

--- keyboards/converter/usb_usb/keymaps/raghu/rules.mk
+++ keyboards/converter/usb_usb/keymaps/raghu/rules.mk
@@ -1,17 +1,18 @@
 F_CPU=8000000
 BOOTLOADER=caterina
-COMBO_ENABLE=no
 EXTRAKEY_ENABLE=yes
 TAP_DANCE_ENABLE=yes
 USB_HID_ENABLE=yes
-LTO_ENABLE=yes
-
-# required to further reduce fw size.
-# EXTRAFLAGS=-flto
 OLED_ENABLE=yes
-MAGIC_ENABLE=no
-
 LEADER_ENABLE=yes
+LTO_ENABLE=yes
+# required to further reduce fw size.
+EXTRAFLAGS=-flto
+SRC+=custom.c
+
+# ----------- disabled for various reasons
+MAGIC_ENABLE=no
+COMBO_ENABLE=no
 SPACE_CADET_ENABLE=no
 
 # pro micro won't boot
@@ -33,4 +34,3 @@ CONSOLE_ENABLE=no
 UNICODE_ENABLE=no
 KEY_OVERRIDE_ENABLE=no
 TERMINAL_ENABLE=no
-SRC+=custom.c
