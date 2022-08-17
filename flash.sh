8ef223a4a2694a3aebfb45bbcec650e38d2923d3
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2022-08-17 08:22:15 +0530

    add check for RAM usage

--- flash.sh
+++ flash.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-QMK=$(qmk env QMK_HOME)
+RAM_LIMIT=2173
 
 pushd keyboards/converter/usb_usb/keymaps/raghu 
 qmk json2c raghu.json > keymap.c
 sed -i '1 a #include "custom.h"' keymap.c
 popd
-RUNTIME=podman util/docker_build.sh converter/usb_usb/pro_micro:raghu:avrdude
-
+RUNTIME=podman util/docker_build.sh converter/usb_usb/pro_micro:raghu
+RAM=$(avr-size -C .build/*_pro_micro_raghu.elf  |grep -Po "Data:\s+\d+" | grep -Po "\d+")
+    echo "=================RAM: .data + .bss=$RAM================================="
+if [ "$RAM" -gt "$RAM_LIMIT" ]; then
+    echo "Your firmware is using too much RAM; ; known good=$RAM_LIMIT "
+    echo "===================================================================="
+    exit 1
+fi
+RUNTIME=podman util/docker_build.sh converter/usb_usb/pro_micro:raghu:flash
 
